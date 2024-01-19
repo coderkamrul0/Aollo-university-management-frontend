b1b2d85ae60541aa7e494c2672a1fab4a9e75c64
kamrul <kamrul444216@gmail.com>
2024-01-20 00:32:22 +0600

    Add: Make dynamic routes & sidebar utils

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,7 +1,8 @@
 import { Layout, Menu } from "antd";
 
 import { Outlet } from "react-router-dom";
-import { adminSidebarItems } from "../../routes/admin.routes";
+import { adminPath } from "../../routes/admin.routes";
+import { sidebarItemGenerator } from "../../utils/sidebarItemsGenerator";
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -34,7 +35,7 @@ const MainLayout = () => {
           theme="dark"
           mode="inline"
           defaultSelectedKeys={["4"]}
-          items={adminSidebarItems}
+          items={sidebarItemGenerator(adminPath, "admin")}
         />
       </Sider>
       <Layout>
