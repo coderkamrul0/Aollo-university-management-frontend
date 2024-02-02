454412c5a069d1b90852f8a62787c4d8d1ee5370
kamrul <kamrul444216@gmail.com>
2024-02-03 02:42:04 +0600

    Add: Show Semester Data Using Ant Design Table

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,9 +1,65 @@
+import { Table, TableColumnsType, TableProps } from "antd";
 import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
 
+interface DataType {
+  _id: string;
+  name: string;
+  year: string;
+  code: string;
+  startMonth: string;
+  endMonth: string;
+}
+
+const columns: TableColumnsType<DataType> = [
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+
+  {
+    title: "Year",
+    dataIndex: "year",
+  },
+  {
+    title: "Start Month",
+    dataIndex: "startMonth",
+  },
+  {
+    title: "End Month",
+    dataIndex: "endMonth",
+  },
+];
+
+const onChange: TableProps<DataType>["onChange"] = (
+  pagination,
+  filters,
+  sorter,
+  extra
+) => {
+  console.log("params", pagination, filters, sorter, extra);
+};
+
 const AcademicSemester = () => {
-  const { data } = useGetAllSemestersQuery(undefined);
-  console.log(data);
-  return <div>AcademicSemester</div>;
+  const { data: semesterData } = useGetAllSemestersQuery(undefined);
+
+  const tableData = semesterData?.data?.map(
+    ({ _id, name, year, startMonth, endMonth }) => ({
+      _id,
+      name,
+      year,
+      startMonth,
+      endMonth,
+    })
+  );
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={tableData}
+      onChange={onChange}
+      rowKey="_id"
+    />
+  );
 };
 
 export default AcademicSemester;
