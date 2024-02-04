a64433890b00990cf49cdd42d3e0f6913f884ba9
kamrul <kamrul444216@gmail.com>
2024-02-05 01:24:48 +0600

    Add: Create academic faculty

--- src/pages/admin/academicManagement/CreateAcademicFaculty.tsx
+++ src/pages/admin/academicManagement/CreateAcademicFaculty.tsx
@@ -1,7 +1,43 @@
-const CreateAcademicFaculty = () => {
-  return (
-    <div>CreateAcademicFaculty</div>
-  )
-}
+import { Button, Col, Flex } from "antd";
+import PHForm from "../../../components/form/PHForm";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { academicFacultySchema } from "../../../schemas/academicManagement.schema";
+import PHInput from "../../../components/form/PHInput";
+import { useAddAcademicFacultyMutation } from "../../../redux/features/admin/academicManagement.api";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import { toast } from "sonner";
 
-export default CreateAcademicFaculty
+const CreateAcademicFaculty = () => {
+  const [addAcademicFaculty] = useAddAcademicFacultyMutation();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("Faculty Creating...");
+
+    const facultyData = {
+      name: data.name,
+    };
+
+    try {
+      await addAcademicFaculty(facultyData);
+      toast.success("Faculty Created.", { id: toastId });
+    } catch (error) {
+      toast.error("Something went wrong.", { id: toastId });
+    }
+  };
+
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm
+          onSubmit={onSubmit}
+          resolver={zodResolver(academicFacultySchema)}
+        >
+          <PHInput type="text" name="name" label="Faculty Name" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
+  );
+};
+
+export default CreateAcademicFaculty;
